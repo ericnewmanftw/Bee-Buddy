d8998d824a8ac9c0c06119b2f298205b8e866ce4
Eric Newman <eric.r.newman87@gmail.com>
2015-04-29 13:53:19 -0600

    Initial Commit

---
+++ Bee Buddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bee Buddy
+//
+//  Created by Eric Newman on 4/29/15.
+//  Copyright (c) 2015 Eric Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bee BuddyTests/Bee_BuddyTests.m
@@ -0,0 +1,40 @@
+//
+//  Bee_BuddyTests.m
+//  Bee BuddyTests
+//
+//  Created by Eric Newman on 4/29/15.
+//  Copyright (c) 2015 Eric Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Bee_BuddyTests : XCTestCase
+
+@end
+
+@implementation Bee_BuddyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
