4829dae9be2a9b9186d1e3bc3851ecb269d29cec
Eric Newman <eric.r.newman87@gmail.com>
2015-05-23 11:35:13 -0600

    Finished organizing core data
    Beginning Hive controller

---
+++ Bee Buddy/HiveController.h
@@ -0,0 +1,13 @@
+//
+//  HiveController.h
+//  Bee Buddy
+//
+//  Created by Eric Newman on 5/23/15.
+//  Copyright (c) 2015 Eric Newman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HiveController : NSObject
+
+@end
---
+++ Bee Buddy/HiveController.m
@@ -0,0 +1,13 @@
+//
+//  HiveController.m
+//  Bee Buddy
+//
+//  Created by Eric Newman on 5/23/15.
+//  Copyright (c) 2015 Eric Newman. All rights reserved.
+//
+
+#import "HiveController.h"
+
+@implementation HiveController
+
+@end
